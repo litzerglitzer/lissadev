b1f0393ad6eccf7518615f7c379eb938c4b4e6a6
litzerglitzer <lissaschwarz@yahoo.de>
2024-05-29 15:04:17 +0200

    home section: introduction mobile

--- lissadev/src/app/pages/home/home.component.ts
+++ lissadev/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IntroductionComponent } from "./introduction/introduction.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CommonModule, IntroductionComponent]
 })
 export class HomeComponent {
 
