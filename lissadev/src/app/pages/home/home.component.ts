dec63c6f3fa3e9d01fc1e55960da8123fb6197d9
litzerglitzer <lissaschwarz@yahoo.de>
2024-05-29 23:50:18 +0200

    add home: work section

--- lissadev/src/app/pages/home/home.component.ts
+++ lissadev/src/app/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IntroductionComponent } from "./introduction/introduction.component";
 import { EducationComponent } from "./education/education.component";
+import { WorkComponent } from "./work/work.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CommonModule, IntroductionComponent, EducationComponent]
+    imports: [CommonModule, IntroductionComponent, EducationComponent, WorkComponent]
 })
 export class HomeComponent {
 
