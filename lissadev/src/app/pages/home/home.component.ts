12665ecd03b0f9d032e95cac10d320de9b96f766
litzerglitzer <lissaschwarz@yahoo.de>
2024-05-29 15:27:04 +0200

    add home education section

--- lissadev/src/app/pages/home/home.component.ts
+++ lissadev/src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IntroductionComponent } from "./introduction/introduction.component";
+import { EducationComponent } from "./education/education.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CommonModule, IntroductionComponent]
+    imports: [CommonModule, IntroductionComponent, EducationComponent]
 })
 export class HomeComponent {
 
