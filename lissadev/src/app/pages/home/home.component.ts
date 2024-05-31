ab31516fe5040a2ac7e53677042730db9749bc36
litzerglitzer <lissaschwarz@yahoo.de>
2024-05-31 14:17:47 +0200

    add home: navigation

--- lissadev/src/app/pages/home/home.component.ts
+++ lissadev/src/app/pages/home/home.component.ts
@@ -4,13 +4,14 @@ import { IntroductionComponent } from "./introduction/introduction.component";
 import { EducationComponent } from "./education/education.component";
 import { WorkComponent } from "./work/work.component";
 import { WorkDevelopementComponent } from "./work-developement/work-developement.component";
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CommonModule, IntroductionComponent, EducationComponent, WorkComponent, WorkDevelopementComponent]
+    imports: [CommonModule, IntroductionComponent, EducationComponent, WorkComponent, WorkDevelopementComponent, NavigationComponent]
 })
 export class HomeComponent {
 
