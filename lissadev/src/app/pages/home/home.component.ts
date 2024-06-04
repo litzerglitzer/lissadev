cb3366d9858386c6e76811a59c3ad6f7b9e0d135
litzerglitzer <lissaschwarz@yahoo.de>
2024-06-05 00:08:18 +0200

    skill tags in work developement section

--- lissadev/src/app/pages/home/home.component.ts
+++ lissadev/src/app/pages/home/home.component.ts
@@ -5,13 +5,14 @@ import { EducationComponent } from "./education/education.component";
 import { WorkComponent } from "./work/work.component";
 import { WorkDevelopementComponent } from "./work-developement/work-developement.component";
 import { NavigationComponent } from "./navigation/navigation.component";
+import { WorkPhotographyComponent } from "./work-photography/work-photography.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CommonModule, IntroductionComponent, EducationComponent, WorkComponent, WorkDevelopementComponent, NavigationComponent]
+    imports: [CommonModule, IntroductionComponent, EducationComponent, WorkComponent, WorkDevelopementComponent, NavigationComponent, WorkPhotographyComponent]
 })
 export class HomeComponent {
 
--- lissadev/src/app/pages/home/work-developement/work-developement.component.ts
+++ lissadev/src/app/pages/home/work-developement/work-developement.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-work-developement',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './work-developement.component.html',
   styleUrl: './work-developement.component.css'
 })
 export class WorkDevelopementComponent {
-
+  skills: string[] = [
+    'Angular',
+    'Typescript',
+    'Firebase',
+    'NoSQL Database',
+    'Firestore Database',
+    'NoSQL Database',
+    'CSS',
+    'Database Architecture',
+    'HTML',
+  ]
 }
