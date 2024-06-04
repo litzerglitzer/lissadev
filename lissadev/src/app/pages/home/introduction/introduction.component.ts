dbb2fb4bb6129f3fc9f25abb2707d99d8b3a80da
litzerglitzer <lissaschwarz@yahoo.de>
2024-06-04 14:32:18 +0200

    add skills introduction

--- lissadev/src/app/pages/home/introduction/introduction.component.ts
+++ lissadev/src/app/pages/home/introduction/introduction.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-introduction',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.css']
 })
 export class IntroductionComponent {
+
+  skills: string[] = [
+    'Angular',
+    'Typescript',
+    'CSS',
+    'Java',
+    'Firebase',
+    'HTML',
+    'SQL'
+  ]
 
 }
