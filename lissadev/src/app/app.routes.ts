85e6228df623ca673c03855b3e5533f425d4e799
litzerglitzer <lissaschwarz@yahoo.de>
2024-06-20 22:46:48 +0200

    Resume

--- lissadev/src/app/app.routes.ts
+++ lissadev/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent}
+    { path: '', component: HomeComponent},
+    { path: 'resume', component:ResumeComponent}
 ];
--- lissadev/src/app/components/header/header.component.ts
+++ lissadev/src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {OverlayModule} from '@angular/cdk/overlay';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule, OverlayModule],
+  imports: [MatIconModule, OverlayModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
