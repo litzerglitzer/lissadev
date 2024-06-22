f85b742b18a7d2b3f21b7cffe434f6cc1eb5e634
litzerglitzer <lissaschwarz@yahoo.de>
2024-06-22 21:39:04 +0200

    add imprint

--- lissadev/src/app/app.routes.ts
+++ lissadev/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ResumeComponent } from './components/resume/resume.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'resume', component:ResumeComponent}
+    { path: 'resume', component:ResumeComponent},
+    { path: 'imprint', component:ImprintComponent}
 ];
